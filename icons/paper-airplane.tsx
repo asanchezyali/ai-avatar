f6bd9a98b219d10d9b2b84a503c355a14340126d
Alejandro Sánchez Yalí <asanchezyali@gmail.com>
2023-12-18 10:36:14 -0500

    add paper airplane icon

---
+++ icons/paper-airplane.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function PaperAirplane() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="1.5"
+      className="w-6 h-6"
+      viewBox="0 0 24 24"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
+      ></path>
+    </svg>
+  );
+}
+
+export default PaperAirplane;
