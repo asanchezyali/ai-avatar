7609caf00147149a5c12c20a4ed957fe2421d5d6
Alejandro Sánchez Yalí <asanchezyali@gmail.com>
2024-01-25 00:02:20 -0500

    update speech synthesis

--- hooks/useSpeechSynthesis.ts
+++ hooks/useSpeechSynthesis.ts
@@ -1,69 +1,37 @@
 import React from "react";
-import * as SpeechSDK from "microsoft-cognitiveservices-speech-sdk";
+import playAudio from "@/utils/playAudio";
 
 export interface VisemeFrame {
   offset: number;
   id: number;
 }
 
+type MessageData = {
+  visemes: VisemeFrame[];
+  filename: string;
+};
+
 export default function useSpeechSynthesis() {
   const [visemeID, setVisemeID] = React.useState(0);
-  const [isDisabled, setIsDisabled] = React.useState(false);
+  const [isPlaying, setIsPlaying] = React.useState(false);
   const [text, setText] = React.useState("");
   const [avatarSay, setAvatarSay] = React.useState("");
-  const synthesizer = React.useRef<SpeechSDK.SpeechSynthesizer | null>(null);
-
-  if (!process.env.NEXT_PUBLIC_SPEECH_KEY || !process.env.NEXT_PUBLIC_SPEECH_REGION) {
-    throw new Error("Speech service credentials are not defined.");
-  }
-
-  const speechConfig = SpeechSDK.SpeechConfig.fromSubscription(
-    process.env.NEXT_PUBLIC_SPEECH_KEY,
-    process.env.NEXT_PUBLIC_SPEECH_REGION
-  );
-
-  React.useEffect(() => {
-    speechConfig.speechSynthesisVoiceName = "en-US-JennyNeural";
-
-    let synthesisStartTime = 0;
-    const player = new SpeechSDK.SpeakerAudioDestination();
-    player.onAudioStart = () => {
-      setIsDisabled(true);
-      synthesisStartTime = Date.now();
-    };
-
-    player.onAudioEnd = () => {
-      setIsDisabled(false);
-    };
-
-    const audioConfig = SpeechSDK.AudioConfig.fromSpeakerOutput(player);
-    synthesizer.current = new SpeechSDK.SpeechSynthesizer(speechConfig, audioConfig);
-
-    synthesizer.current.synthesisCompleted = function (s, e) {
-      setIsDisabled(false);
-    };
-
-    synthesizer.current.visemeReceived = (s, e) => {
-      const visemeTime = synthesisStartTime + e.audioOffset / 10000;
-      const delay = visemeTime - Date.now();
-      setTimeout(
-        () => {
-          setVisemeID(e.visemeId);
-        },
-        delay > 0 ? delay + 500 : 0
-      );
-    };
-  }, [speechConfig]);
+  const [messageData, setMessageData] = React.useState<MessageData | null>(null);
 
   const handleTextChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setText(event.target.value);
   };
 
   const handleSynthesis = async () => {
+    if (isPlaying) {
+      return;
+    }
+    setIsPlaying(true);
+    setMessageData(null);
     setAvatarSay("Please wait...");
     setText("");
 
-    const response = await fetch("/api/openai", {
+    const response = await fetch("/api/server", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -80,32 +48,16 @@ export default function useSpeechSynthesis() {
     const messageFromOpenAI = data.response;
 
     setAvatarSay(messageFromOpenAI);
-
-    const ssml = `<speak version="1.0"
-        xmlns="http://www.w3.org/2001/10/synthesis"
-        xmlns:mstts="https://www.w3.org/2001/mstts"
-        xml:lang="en-US">
-        <voice name="en-US-JennyNeural">
-            <mstts:viseme type="redlips_front"/>
-            <break time="500ms"/>
-            <mstts:express-as style="excited">
-                <prosody rate="-8%" pitch="23%">
-                    ${messageFromOpenAI}
-                </prosody>
-            </mstts:express-as>
-            <mstts:viseme type="sil"/>
-            <mstts:viseme type="sil"/>
-        </voice>
-      </speak>`;
-
-    if (!synthesizer.current) {
-      return;
-    }
-
-    synthesizer.current.speakSsmlAsync(ssml, (error) => {
-      console.log(error);
-    });
+    setMessageData(data);
   };
 
-  return { visemeID, setVisemeID, isDisabled, text, avatarSay, handleTextChange, handleSynthesis };
+  React.useEffect(() => {
+    if (isPlaying && messageData) {
+      playAudio({ setVisemeID, visemes: messageData.visemes, audioFile: messageData.filename }).then(() => {
+        setIsPlaying(false);
+      });
+    }
+  }, [isPlaying, messageData]);
+
+  return { visemeID, setVisemeID, isPlaying, text, avatarSay, handleTextChange, handleSynthesis };
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,10 @@
 import React from "react";
 import ZippyAvatar from "@/components/avatar/ZippyAvatar";
 import PaperAirplane from "@/components/icons/PaperAirplane";
-import InformationCircle from "@/components/icons/InformationCircle";
-import playAudio from "@/utils/playAudio";
 import useSpeechSynthesis from "@/hooks/useSpeechSynthesis";
 
-export default function AvatarApp() { 
-  const { visemeID, setVisemeID, isDisabled, text, avatarSay, handleTextChange, handleSynthesis } =
-    useSpeechSynthesis();
+export default function AvatarApp() {
+  const { visemeID, isPlaying, text, avatarSay, handleTextChange, handleSynthesis } = useSpeechSynthesis();
   return (
     <div className="w-screen h-screen items-center justify-center flex flex-col mx-auto">
       <div className="flux justify-center items-center w-[500px] relative">
@@ -20,17 +17,11 @@ export default function AvatarApp() {
         <h1 className="text-2xl font-bold text-center text-blue-600">Zippy Talking Avatar</h1>
       </div>
       <div className="h-10 relative my-4">
-        <button
-          className="bg-blue-500 text-white font-bold py-2 px-3 rounded-l-lg absolute bottom-0 left-0 w-[50px] h-10"
-          onClick={() => playAudio(setVisemeID)}
-        >
-          <InformationCircle />
-        </button>
         <input
           type="text"
           value={text}
           onChange={handleTextChange}
-          className="border-2 border-gray-300 bg-gray-100 h-10 w-[600px] pl-[60px] pr-[120px] rounded-lg text-sm mb-2"
+          className="border-2 border-gray-300 bg-gray-100 h-10 w-[600px] pl-[20px] pr-[120px] rounded-lg text-sm mb-2"
           placeholder="Write something..."
           maxLength={100}
           onKeyDown={(event) => {
@@ -40,9 +31,12 @@ export default function AvatarApp() {
           }}
         />
         <button
-          className="bg-blue-500 text-white font-bold py-2 px-3 rounded-r-lg absolute bottom-0 right-0 w-[50px] h-10"
+          className={`
+            bg-blue-500 text-white font-bold py-2 px-3 rounded-r-lg absolute bottom-0 right-0 w-[50px] h-10 
+            ${isPlaying ? "bg-gray-500 cursor-not-allowed" : "bg-blue-500 text-white"}
+          `}
           onClick={() => handleSynthesis()}
-          disabled={isDisabled}
+          disabled={isPlaying}
         >
           <PaperAirplane />
         </button>
